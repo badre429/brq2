04da9a7a38a6ac55c85ea65752b1dacdb328148a
badre429@hotmail.com <badre429@hotmail.com>
2020-05-11 21:49:10 +0100

    UPDATE ASSETS

--- wwwroot/index.js
+++ wwwroot/index.js
@@ -9,12 +9,12 @@ var rect = {
   isDown: false,
   serverDownloading: true,
   c1: { lat: 0, lng: 0 },
-  c2: { lat: 0, lng: 0 }
+  c2: { lat: 0, lng: 0 },
 };
 function getStatus() {
   if (rect.serverDownloading) {
-    fetch('/api/Tile/DownloadState').then(e =>
-      e.json().then(json => {
+    fetch("/api/Tile/DownloadState").then((e) =>
+      e.json().then((json) => {
         // console.log(json);
         vueApp.serverStat = json;
         vueApp.serverDownloading = true;
@@ -29,45 +29,52 @@ function getStatus() {
 }
 
 function createSelectLayer() {
-  map.addSource('selected-rect', {
-    type: 'geojson',
+  map.addSource("selected-rect", {
+    type: "geojson",
     data: {
-      type: 'FeatureCollection',
+      type: "FeatureCollection",
       features: [
         {
-          type: 'Feature',
+          type: "Feature",
           geometry: {
-            type: 'Polygon',
-            // coordinates: [[[-10, 37.5], [-10, 18], [18, 18], [18, 37.5], [-10, 37.5]]]
-            coordinates: [[[-10, 37.5], [-10, 37.5], [-10, 37.5], [-10, 37.5], [-10, 37.5]]]
-          }
-        }
-      ]
-    }
+            type: "Polygon",
+            coordinates: [
+              [
+                [-10, 37.5],
+                [-10, 37.5],
+                [-10, 37.5],
+                [-10, 37.5],
+                [-10, 37.5],
+              ],
+            ],
+          },
+        },
+      ],
+    },
   });
   map.addLayer({
-    id: 'selected-rect',
-    type: 'fill',
-    source: 'selected-rect',
+    id: "selected-rect",
+    type: "fill",
+    source: "selected-rect",
     paint: {
-      'fill-color': '#0000FF',
-      'fill-opacity': 0.2
+      "fill-color": "#0000FF",
+      "fill-opacity": 0.2,
     },
     layout: {
-      visibility: 'visible'
-    }
+      visibility: "visible",
+    },
   });
   map.addLayer({
-    id: 'selected-rect-outline',
-    type: 'line',
-    source: 'selected-rect',
+    id: "selected-rect-outline",
+    type: "line",
+    source: "selected-rect",
     paint: {
-      'line-color': '#0000FF',
-      'line-width': 2
+      "line-color": "#0000FF",
+      "line-width": 2,
     },
     layout: {
-      visibility: 'visible'
-    }
+      visibility: "visible",
+    },
   });
 }
 
@@ -75,26 +82,36 @@ getStatus();
 function createMapStyle(p) {
   return {
     version: 8,
-    // glyphs: '/api/{fontstack}-{range}.pbf',
+    glyphs: "/outlib/glyphs/{fontstack}-{range}.pbf",
     sources: {
-      'simple-tiles': {
-        type: 'raster',
-        tiles: ['/tile/{z}/{x}/{y}?layerId=' + p.layers[0].id],
-        tileSize: 256
-      }
+      "simple-tiles": {
+        type: "raster",
+        tiles: ["/tile/{z}/{x}/{y}?layerId=" + p.layers[0].id],
+        tileSize: 256,
+      },
     },
     layers: [
       {
-        id: 'simple-tiles',
-        type: 'raster',
-        source: 'simple-tiles',
+        id: "simple-tiles",
+        type: "raster",
+        source: "simple-tiles",
         minzoom: 0,
-        maxzoom: 22
-      }
-    ]
+        maxzoom: 22,
+      },
+    ],
   };
 }
+function getLayerFrom(v, layer) {
+  if (v == null) {
+    return "/mapstyles/" + layer + ".json";
+  } else {
+    var style = createMapStyle(v);
+    // console.log('raster data type', this.layer, v, style);
+    return style;
+  }
+}
 function CreateLayer(prv) {
+  // @ts-ignore
   var layer = prv.layer[0];
 }
 function updateSelectedLayer() {
@@ -103,63 +120,130 @@ function updateSelectedLayer() {
   var StartLng = Math.min(rect.c1.lng, rect.c2.lng);
   var EndLng = Math.max(rect.c1.lng, rect.c2.lng);
   var data = {
-    type: 'FeatureCollection',
+    type: "FeatureCollection",
     features: [
       {
-        type: 'Feature',
+        type: "Feature",
         geometry: {
-          type: 'Polygon',
+          type: "Polygon",
           coordinates: [
             [
               [StartLng, StartLat],
               [StartLng, EndLat],
               [EndLng, EndLat],
               [EndLng, StartLat],
-              [StartLng, StartLat]
-            ]
-          ]
-        }
-      }
-    ]
+              [StartLng, StartLat],
+            ],
+          ],
+        },
+      },
+    ],
   };
-  map.getSource('selected-rect').setData(data);
+  map.getSource("selected-rect").setData(data);
 }
-fetch('/api/Tile/Providers').then(e =>
-  e.json().then(c => {
-    // console.log(c);
+function DrawPointsLayer() {
+  map.addSource("geojsonPoints", {
+    type: "geojson",
+    data: "/geo.json",
+  });
+  map.addLayer({
+    id: "points",
+    type: "circle",
+    source: "geojsonPoints",
+    paint: {
+      "circle-radius": 5,
+      "circle-color": [
+        "case",
+        ["==", ["get", "country"], "DZ"],
+        "green",
+        "blue",
+      ],
+    },
+  });
+  map.addLayer({
+    id: "labelspoints",
+    type: "symbol",
+    source: "geojsonPoints",
+    layout: {
+      "text-font": ["Arial"],
+      "text-size": {
+        base: 1.2,
+        stops: [
+          [7, 12],
+          [11, 19],
+        ],
+      },
+      "text-offset": [0, 0.5],
+      "text-anchor": "top",
+      "text-field": {
+        stops: [
+          [0, ""],
+          [7, "{name}"],
+        ],
+      },
+    },
+    paint: {
+      "text-color": "#0000ff",
+      "text-halo-width": 3,
+      "text-halo-blur": 2,
+      "text-halo-color": "rgba(255,255,255,0.8)",
+    },
+  });
+}
+fetch("/api/Tile/Providers").then((e) =>
+  e.json().then((c) => {
     mapboxgl.accessToken =
-      'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+      "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA";
+    var style = null;
+    if (localStorage.getItem("selected-layer") != null) {
+      var v = c.find(
+        (k) =>
+          k.name == localStorage.getItem("selected-layer") && k.type == "raster"
+      );
+      style = getLayerFrom(v, localStorage.getItem("selected-layer"));
+    } else style = "/mapstyles/Mapbox.json";
+    var l = null;
+    if (localStorage.getItem("oldMap")) {
+      var l = JSON.parse(localStorage.getItem("oldMap"));
+    }
     map = new mapboxgl.Map({
-      container: 'main-map', // container id
-      style: '/mapstyles/Mapbox.json',
-      center: [3.5, 29], // starting position
-      zoom: 4.5 // starting zoom
+      container: "main-map", // container id
+      // @ts-ignore
+      style: style,
+      center: l ? [l.lng, l.lat] : [3.5, 29], // starting position
+      zoom: l ? l.zoom : 4.5, // starting zoom
     });
-    map.on('load', k => {
+    // @ts-ignore
+    map.on("load", (k) => {
       createSelectLayer();
+      if (localStorage.getItem("oldMap")) {
+        var l = JSON.parse(localStorage.getItem("oldMap"));
+      }
     });
-    map.on('mousedown', k => {
+    map.on("mousedown", (k) => {
       rect.c1 = k.lngLat;
       var e = k.originalEvent;
       if (rect.started && !rect.isDown && e.button == 0) {
         rect.isDown = true;
-        var elm = document.getElementById('select-rect');
+        // @ts-ignore
+        var elm = document.getElementById("select-rect");
         // elm.style.display = 'unset';
-        if (map.getLayer('selected-rect-outline') == null) {
+        if (map.getLayer("selected-rect-outline") == null) {
           createSelectLayer();
         }
         map
-          .getLayer('selected-rect-outline')
-          .setLayoutProperty('visibility', 'visible');
+          .getLayer("selected-rect-outline")
+          .setLayoutProperty("visibility", "visible");
         map
-          .getLayer('selected-rect')
-          .setLayoutProperty('visibility', 'visible');
+          .getLayer("selected-rect")
+          .setLayoutProperty("visibility", "visible");
         rect.p1 = { x: e.clientX, y: e.clientY };
         map.dragPan.disable();
         //   console.log(e);
       }
     });
-    window.addEventListener('mouseup', k => {
+    // @ts-ignore
+    window.addEventListener("mouseup", (k) => {
       if (rect.started && rect.isDown) {
         rect.isDown = false;
         vueApp.rect = rect;
@@ -171,31 +255,42 @@ fetch('/api/Tile/Providers').then(e =>
       }
     });
 
-    map.on('mousemove', k => {
+    map.on("mousemove", (k) => {
+      // @ts-ignore
       var e = k.originalEvent;
       if (rect.started && rect.isDown) {
         rect.c2 = k.lngLat;
         //   console.log(e);
 
-        if (map.getLayer('selected-rect-outline') == null) {
+        if (map.getLayer("selected-rect-outline") == null) {
           createSelectLayer();
         }
         map
-          .getLayer('selected-rect-outline')
-          .setLayoutProperty('visibility', 'visible');
+          .getLayer("selected-rect-outline")
+          .setLayoutProperty("visibility", "visible");
         map
-          .getLayer('selected-rect')
-          .setLayoutProperty('visibility', 'visible');
+          .getLayer("selected-rect")
+          .setLayoutProperty("visibility", "visible");
         updateSelectedLayer();
       }
     });
     // Add zoom and rotation controls to the map.
+
+    map.on("moveend", () => {
+      var l = map.getCenter();
+      l.zoom = map.getZoom();
+      console.log(l);
+      localStorage.setItem("oldMap", JSON.stringify(l));
+    });
     map.addControl(new mapboxgl.NavigationControl());
+    map.addControl(new mapboxgl.ScaleControl());
     vueApp = new Vue({
-      el: '#main-toolbar',
+      el: "#main-toolbar",
+      // @ts-ignore
+      vuetify: new Vuetify(),
       data: {
-        layer: null,
-        items: c,
+        layer: localStorage.getItem("selected-layer") || "Mapbox",
+        items: [...c],
         selection: false,
         serverDownloading: false,
         cancelMessage: false,
@@ -204,76 +299,161 @@ fetch('/api/Tile/Providers').then(e =>
         serverStat: {
           zoom: 0,
           count: 0,
-          total: 0
+          total: 0,
         },
-        rect: {}
+        rect: {},
       },
       methods: {
-        startSelection: function(e) {
+        // @ts-ignore
+        startSelection: function (e) {
           this.regionSelected = false;
-          var elm = document.getElementById('select-rect');
+          var elm = document.getElementById("select-rect");
           // elm.style.display = 'none';
 
-          if (map.getLayer('selected-rect-outline') == null) {
+          if (map.getLayer("selected-rect-outline") == null) {
             createSelectLayer();
           }
           // map.getLayer('selected-rect').setLayoutProperty('visibility', 'none');
           // map
           //   .getLayer('selected-rect-outline')
           //   .setLayoutProperty('visibility', 'none');
-          elm.style.height = '0px';
-          elm.style.width = '0px';
+          elm.style.height = "0px";
+          elm.style.width = "0px";
           rect.started = true;
           this.selection = true;
           // console.log(this);
         },
-        endSelection: function(e) {
+        DrawPointsLayer: function () {
+          DrawPointsLayer();
+        },
+        // @ts-ignore
+        endSelection: function (e) {
           // document.getElementById('select-rect').style.display = 'none';
           rect.started = false;
           this.selection = false;
           // console.log(this);
         },
 
-        layerChanged: function(e) {
-          var v = c.find(k => k.name == this.layer && k.type == 'raster');
-          if (v == null) {
-            map.setStyle('/mapstyles/' + this.layer + '.json');
-          } else {
-            var style = createMapStyle(v);
-            // console.log('raster data type', this.layer, v, style);
-            map.setStyle(style);
-          }
+        // @ts-ignore
+        layerChanged: function (e) {
+          localStorage.setItem("selected-layer", this.layer);
+          var v = c.find((k) => k.name == this.layer && k.type == "raster");
+
+          map.setStyle(getLayerFrom(v, this.layer));
         },
-        startDownloading: function() {
+        startDownloading: function () {
           var self = this;
-          var v = c.find(k => k.name == this.layer);
+          var v = c.find((k) => k.name == this.layer);
           rect.providerId = v.id;
           rect.startZoom = Math.floor(map.getZoom());
-          rect.endZoom = 20;
-          fetch('/api/Tile/StartDownloading', {
-            method: 'POST',
+          rect.endZoom = 17;
+          delete rect["points"];
+          fetch("/api/Tile/StartDownloading", {
+            method: "POST",
             headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json'
+              Accept: "application/json",
+              "Content-Type": "application/json",
             },
-            body: JSON.stringify(rect)
-          }).then(k => {
+            body: JSON.stringify(rect),
+            // @ts-ignore
+          }).then((k) => {
             rect.serverDownloading = true;
             self.serverDownloading = true;
           });
         },
-        updateSelectedLayer: function() {
+        startDownloadingPoints: function () {
+          var self = this;
+          var v = c.find((k) => k.name == this.layer);
+          rect.providerId = v.id;
+          rect.startZoom = Math.floor(map.getZoom());
+          rect.endZoom = 17;
+          fetch("/geo.json").then((k) =>
+            k.json().then((data) => {
+              console.log(data);
+              rect.points = data.features
+                .map((k) => k.geometry.coordinates)
+                .map((cr) => ({ lat: cr[1], lng: cr[0] }))
+                .filter((cr) => cr.lat != null && cr.lng != null);
+              console.log(rect);
+              fetch("/api/Tile/StartDownloading", {
+                method: "POST",
+                headers: {
+                  Accept: "application/json",
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(rect),
+                // @ts-ignore
+              }).then((k) => {
+                rect.serverDownloading = true;
+                self.serverDownloading = true;
+              });
+            })
+          );
+        },
+        updateSelectedLayer: function () {
           updateSelectedLayer();
         },
-        stopDownloading: function() {
+        stopDownloading: function () {
           var self = this;
-          fetch('/api/Tile/DownloadCancel').then(k => {
+          // @ts-ignore
+          fetch("/api/Tile/DownloadCancel").then((k) => {
             self.cancelMessage = true;
             rect.serverDownloading = false;
             self.serverDownloading = false;
           });
-        }
-      }
+        },
+      },
     });
+
+    // @ts-ignore
+    var draw = new MapboxDraw({
+      // displayControlsDefault: false,
+      // controls: {
+      //   polygon: true,
+      //   trash: true,
+      // },
+    });
+    map.on("moveend", () => {
+      var l = map.getCenter();
+      l.zoom = map.getZoom();
+      console.log(l);
+      localStorage.setItem("mapOld", JSON.stringify(l));
+    });
+    map.addControl(draw, "bottom-right");
+    map.on("draw.create", updateArea);
+    map.on("draw.delete", updateArea);
+    map.on("draw.update", updateArea);
+    // map.on("draw.actionable", updateArea);
+    // map.on("click", (e) => updateArea(e, true));
+    var isDrawwing = 0;
+    function updateArea(e, click) {
+      var data = draw.getAll();
+      console.log(data);
+      var answer = document.getElementById("calculated-area");
+      if (data.features.length > 0) {
+        var feat = data.features[data.features.length - 1];
+        if (feat.geometry.type == "Polygon") {
+          // @ts-ignore
+          var area = turf.area(data);
+          // restrict to area to 2 decimal points
+          var rounded_area = Math.round(area * 100) / 100;
+          answer.innerHTML =
+            "<p><strong>surface:" +
+            rounded_area +
+            "</strong></p><p>square meters</p>";
+        } else if (feat.geometry.type == "LineString") {
+          // @ts-ignore
+          var area = turf.length(data);
+          // restrict to area to 2 decimal points
+          var rounded_area = Math.round(area * 1000) / 1000;
+          answer.innerHTML =
+            "<p><strong>distance" + rounded_area + "</strong></p><p> km</p>";
+        }
+      } else {
+        answer.innerHTML = "";
+        if (e.type !== "draw.delete")
+          alert("Use the draw tools to draw a polygon!");
+      }
+    }
   })
 );
